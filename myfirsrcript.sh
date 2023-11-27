557662c5e7fc5bbf9c78416ca2711ecbb1b6d64d
isabellalejandra <isabellabarrera9@gmail.com>
2023-11-27 13:29:08 -0800

    install changes -y

--- myfirsrcript.sh
+++ myfirsrcript.sh
@@ -1,8 +1,12 @@
 # installing postgressql
-sudo apt install postgresql-server-dev-all
+sudo apt install postgresql-server-dev-all -y
 # installing gnupg 
-sudo apt install gnupg
+sudo apt install gnupg -y 
 # installing curl 
-sudo apt install curl
+sudo apt install curl -y 
 #installing apache2
-sudo apt install apache2
+sudo apt install apache2 -y 
+# installing mysql-server-8.0
+sudo apt install mysql-server-8.0 -y
+
+
